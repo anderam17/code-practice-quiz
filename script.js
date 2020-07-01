e81b6f72389791b4be0397ef3135d462ffc9d3e5
Allana Anderson <allana.anderson17@gmail.com>
2020-06-30 21:09:36 -0600

    commented everything out an uncommented one small section at a time. Currently working: Starter page appears correctly. Once submit button pushed, heading is replaced with first question, paragraph and button dissapears, answer choices appear. Clicking an answer choice takes you to the next answer choice and question.

--- script.js
+++ script.js
@@ -1,74 +1,111 @@
 var questions = []
+var userScore = 0;
+var currentQuestion = 0;
+var secondsLeft = 60;
 
 questions[0] = {
     question: "Commonly used data types DO NOT include:",
     choices: ["Strings", "Booleans", "Alerts", "Numbers"],
-    correctAnswer: 3,
+    correctAnswer: "Alerts",
 };
 questions[1] = {
     question: "String values must be enclosed within _______ when being assigned to variables.",
     choices: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
-    correctAnswer: 3,
+    correctAnswer: "Quotes",
 };
 questions[2] = {
     question: "Arrays in JavaScript can be used to store _______.",
     choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
-    correctAnswer: 4,
+    correctAnswer: "All of the Above",
 };
 questions[3] = {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["JavaScript", "Terminal/Bash", "For Loops", "Console.log"],
-    correctAnswer: 4,
+    correctAnswer: "Console.log",
 }
 questions[4] = {
     question: "The condition in an if/else statement is enclosed within ________",
     choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
-    correctAnswer: 3,
+    correctAnswer: "Parentheses",
 }
 
 var button = document.querySelector("#quiz-start");
 var timer = document.querySelector(".timer");
 var main = document.querySelector(".main")
 var header = document.querySelector("#question");
-var answerOptions = document.querySelector("ol")
-var paragraph = document.body.querySelector("p");
+var answerChoices = document.querySelector("ol")
+var paragraph = document.querySelector("#intro");
 
-var secondsLeft = 60;
 
-//Function that goes from one question to another
-//function that starts timer in the top right corner
-//on click function for the Start quiz button that creates first question, starts timer, 
-//add click event on all of main
+// function checkAnswer(arrayIndex){
+//     answerOptions.addEventListener("click", function(event){
+//         if (currentQuestion === 4 || secondsLeft === 0){
+//             console.log("done");    //check if this works
+//             console.log(currentQuestion);
 
-//Need to solidify the change from welcome screen to first question
-button.addEventListener("click", function(){
+//         } else if (event.target === questions[arrayIndex].correctAnswer)
+//         userScore++;
+//         //alert that they got it right
+//         // else{
+//             //     //alert that they got it wrong
+//             // }
+
+//             createQuestion(currentQuestion)
+//         })
+
+//     }
     
-    //!need to also go to screen that tells you your score
-    //!need another if statement that checks if they got it wrong and takes away 10 seconds
-    //!For some reason there is a delay before the timer starts
-    var timeInterval = setInterval(function(){
-        secondsLeft--;
-        timer.textContent = "Timer: " + secondsLeft;
-        
-        if(secondsLeft === 0){
-            clearInterval(timeInterval);
+    function choicesCreator(arrayIndex){
+        answerChoices.textContent = "";
+        for(var i=0; i<4; i++){
+            var listItem = document.createElement("li");
+            listItem.textContent = questions[arrayIndex].choices[i];
+            answerChoices.appendChild(listItem);
         }
-
-    }, 1000)
-    
-    //! This is important and well done (if I do say so myself). Need to repeat this for each quetion. Make it's own method?
-    header.textContent = questions[1].question;
-    choicesCreator(1);
-})
-
-function choicesCreator(arrayIndex){
-    for(var i=0; i<4; i++){
-        var listItem = document.createElement("li");
-        listItem.textContent = questions[arrayIndex].choices[i];
-        answerOptions.appendChild(listItem);
+        currentQuestion++;
     }
-}
+    
+//     //Creates a full question by pulling from the questions array using the passed array index
+    function createQuestion(arrayIndex){
+        header.textContent = questions[arrayIndex].question;
+        choicesCreator(arrayIndex);
+    }
+    
 
+    //This is where actual actions start
+    button.addEventListener("click", function(){
+        createQuestion(0);
+        button.setAttribute("style", "display: none");
+        paragraph.setAttribute("style", "display: none");
+        
+        // var timeInterval = setInterval(function(){
+        //     secondsLeft--;
+        //     timer.textContent = "Timer: " + secondsLeft;
+            
+        //     if(secondsLeft === 0){
+        //         clearInterval(timeInterval);
+        //         header.textContent = "All done!"
+        //         paragraph.textContent = "Your final score is "  //Need to add up score
+        //     }
+            
+        // }, 1000)
+    })
+    answerChoices.addEventListener("click", function(){
+        createQuestion(currentQuestion);
+    })
+    
+//     for(var i=0; i<questions.length; i++){
+//         checkAnswer(i);
+//         currentQuestion++;
+//         console.log(userScore);    //check if it's tracking score
+//     }
 
+// //Function that goes from one question to another
+// //add click event on ordered list
+// //Need to solidify the change from welcome screen to first question
+// //get rid of start quiz button
 
-// Maybe save answer options in an object?
+// //!need to also go to screen that tells you your score    
+// //!need another if statement that checks if they got it wrong and takes away 10 seconds
+// //!For some reason there is a delay before the timer starts
+// //! Cannot get rid of paragraph for some reason
